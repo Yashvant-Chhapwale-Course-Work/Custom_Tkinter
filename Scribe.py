24861005a9911400b001e7c927201ee7fa583676
Yashvant-Chhapwale <yash.chhapwale@gmail.com>
2025-01-27 16:37:55 +0530

    Scribe_Smart Notes

--- Scribe.py
+++ Scribe.py
@@ -76,7 +76,7 @@ class Scribe(ctk.CTk):
         self.add_status_bar()
 
         # Initialize "genai" Model
-        gemini_key="PASTE_API_KEY_HERE" # Store the API Key in the variable.
+        gemini_key="PASTE_API_KEY_HERE" # Store the API Key in the variable. 
         genai.configure(api_key = gemini_key)
         self.model = genai.GenerativeModel('gemini-pro')
 
